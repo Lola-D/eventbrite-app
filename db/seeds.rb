657afcee1e5ca502f70bd88cf1aba86657eea29c
Lola-D <lola.donval@gmail.com>
2020-02-17 19:06:53 +0100

    mailers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+User.reset_pk_sequence
+Event.reset_pk_sequence
+Attendance.reset_pk_sequence
+
+20.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 4), email: Faker::Internet.unique.email(domain: "yopmail.com"), encrypted_password: "123456")
+end
+
+10.times do
+  Event.create(start_date: Faker::Time.forward(days: 60, format: :long), duration: 120, title: Faker::Marketing.buzzwords, description: Faker::Lorem.paragraph(sentence_count: 4), price: Faker::Number.within(range: 1..100), location: Faker::Address.city, admin: User.all.sample)
+end
+
+5.times do
+  Attendance.create(stripe_customer_id: User.all.sample, event: Event.all.sample, participant: User.all.sample)
+end
