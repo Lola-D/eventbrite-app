8b5581653d6aade3e0289b9b780837b26cb266e9
Lola-D <lola.donval@gmail.com>
2020-02-18 18:56:00 +0100

    ...

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,26 @@
 class AttendancesController < ApplicationController
+
+  # def new
+  #   @event = Event.find(params[:id])
+  # end
+
+  # def create
+  #   @event = Event.find(params['event_id'])
+  #   @participant = current_user.id
+  #   @attendance = @event.attendances.create(attendance_params)
+  #   if @attendance.save
+  #     flash[:success] = "Le Commentaire a bien été créé !"
+  #     redirect_to event_path(@event.id)
+  #   else
+  #     flash[:danger] = "Le Commentaire n'a pas été créé !"
+  #     render event_path(@event.id)
+  #   end
+  # end
+
+  # private
+
+  # def attendance_params
+  #   params.require(:attendance).permit(:stripe_customer_id))
+  # end
+
 end
