52c9dce1d997b8ccf208dd1d3bf6a7551f3ccb4e
Lola-D <lola.donval@gmail.com>
2020-02-19 19:24:48 +0100

    admin last

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,12 @@
 module UsersHelper
+
   def is_admin
     @event = Event.find(params[:id])
     !current_user.nil? && current_user.id == @event.admin_id
   end
+
+  def current_user_profil
+    current_user.id.to_i == params[:id].to_i
+  end
+
 end
