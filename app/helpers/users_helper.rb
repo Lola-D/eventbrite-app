0b55336d143fb30976c3c35c62ff92e0f8f6fb75
Lola-D <lola.donval@gmail.com>
2020-02-19 18:00:23 +0100

    admin

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
   def is_admin
-    !current_user.nil? && current_user.id == @event.admin.id
+    @event = Event.where(admin_id: 'user.id')
+    !current_user.nil? && current_user.id == @event.admin_id
   end
 end
