8ad564891468555ad6fca7fe9bfffcb8723e7fd9
Lola-D <lola.donval@gmail.com>
2020-02-20 14:19:42 +0100

    profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_devise_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   include UsersHelper
 
+  protected
+  
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :current_password)}
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,25 +7,4 @@ class UsersController < ApplicationController
     @user = User.find(params['id'])
   end
 
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to user_path(@user.id)
-      flash[:success] = "Ton profil a bien été édité !"
-    else
-      render edit_user_path(@user.id)
-      flash[:danger] = "Ton profil n'a pas été édité !"
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :description)
-  end
-
 end
